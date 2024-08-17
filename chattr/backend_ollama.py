9b068f70ab4b55aaca56113cb7add1cdb8d8e717
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-17 16:48:45 -0500

    Moves ollama code to its own script

---
+++ chattr/backend_ollama.py
@@ -0,0 +1,37 @@
+from requests import post
+from json import dumps, loads
+from sys import stdout
+
+def _ch_submit_ollama(prompt, stream = True, history = [], preview = False):
+    url = "http://localhost:11434/api/chat"
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    messages = []
+    messages.append(dict(
+        role =  "system", 
+        content = "You are a helpful coding assistant that uses Python for data analysis. Keep comments to a minimum."
+        ))
+    messages.append(dict(
+        role =  "user", 
+        content = prompt
+        ))
+
+    data = {
+        'model': 'llama2',
+        'messages' :  messages, 
+        'stream': stream
+        }
+
+    if preview:    
+        print(data)
+        return()
+
+    response = post(url, data = dumps(data), headers = headers, stream = stream)
+    for line in response.iter_lines():
+        body = loads(line)
+        resp = body.get("message")
+        content = resp.get("content")
+        stdout.write(content)
--- chattr/prompt.py
+++ chattr/prompt.py
@@ -1,6 +1,4 @@
-from requests import post
-from json import dumps, loads
-from sys import stdout
+from .backend_ollama import _ch_submit_ollama
 
 def chat(prompt, stream = True, history = [], preview = False):
     return(
@@ -11,38 +9,3 @@ def chat(prompt, stream = True, history = [], preview = False):
             preview = preview
             )
         )
-
-def _ch_submit_ollama(prompt, stream = True, history = [], preview = False):
-    url = "http://localhost:11434/api/chat"
-
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    messages = []
-    messages.append(dict(
-        role =  "system", 
-        content = "You are a helpful coding assistant that uses Python for data analysis. Keep comments to a minimum."
-        ))
-    messages.append(dict(
-        role =  "user", 
-        content = prompt
-        ))
-
-    data = {
-        'model': 'llama2',
-        'messages' :  messages, 
-        'stream': stream
-        }
-
-    if preview:    
-        print(data)
-        return()
-
-    response = post(url, data = dumps(data), headers = headers, stream = stream)
-    for line in response.iter_lines():
-        body = loads(line)
-        resp = body.get("message")
-        content = resp.get("content")
-        stdout.write(content)
-
