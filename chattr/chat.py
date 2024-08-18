bac9b34fbdfd23230d509d74eb6258edef94ec31
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-18 14:58:50 -0500

    Adds automated port selection

--- chattr/chat.py
+++ chattr/chat.py
@@ -1,5 +1,6 @@
 from .backend_ollama import _ch_submit_ollama
 from os import path
+import socket
 import subprocess
 
 def chat(prompt, stream = True, history = [], preview = False):
@@ -13,12 +14,16 @@ def chat(prompt, stream = True, history = [], preview = False):
         )
 
 def app():
-    app_dir = path.dirname(chattr.__file__)
+    sock = socket.socket()
+    sock.bind(('', 0))
+    port = sock.getsockname()[1]
+    app_dir = path.dirname(__file__)
     app_file = path.join(app_dir, "app.py")
     args = [
     'shiny',
     'run', 
-    app_file
+    app_file,
+    '--port=' + str(port)
     ]
     proc = subprocess.Popen(
         args,
