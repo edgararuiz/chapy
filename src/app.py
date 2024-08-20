b313d2279c793560592d723078fb7fb376fe68b8
Edgar Ruiz <edgararuiz@gmail.com>
2024-08-20 10:28:24 -0500

    Begins dark mode support

--- src/app.py
+++ src/app.py
@@ -16,14 +16,17 @@ app_ui = ui.page_fluid(
     ui.tags.style(".bslib-gap-spacing { padding:4px; font-size:90%;} "),
     ui.tags.style(".bslib-mb-spacing { padding:1px; margin:1px;}"),
     ui.tags.style(".bslib-grid-item { padding:1px;}"),
-    ui.tags.style(".bslib-grid { padding:1px;}"),
+    ui.tags.style(".bslib-grid { padding:10px;}"),
     ui.layout_columns(
+      ui.div(
+        ui.input_dark_mode(), 
+      ),        
       ui.input_text_area("prompt", "", width="100%", resize=False),
       ui.div(
-        ui.input_task_button("submit", "Submit", style = ui_general("font-size:65%;")), 
-        ui.input_task_button("options", "Options", style = ui_general("font-size:65%;"))
+        ui.input_task_button("submit", "Submit", style = ui_general("font-size:65%; padding:4px; margin:2px")), 
+        #ui.input_task_button("options", "Options", style = ui_general("font-size:65%; padding:3px; margin:2px;"))
       ),
-      col_widths= (10, 1)
+      col_widths= (1, 10, 1)
     ),
     ui.layout_columns(
         ui.output_ui("value"),
@@ -99,7 +102,10 @@ def server(input: Inputs, output: Outputs, session: Session):
                 if response != '':    
                     ui.insert_ui(                        
                         ui.layout_columns(
-                            ui.card(ui.markdown(response), style = "padding:0; margin:0"),
+                            ui.card(
+                                ui.markdown(response), 
+                                style = "padding:0; margin:0; border-color: #ccc;"
+                                ),
                             ui.p(),
                             col_widths= (11, 1)
                             ), 
